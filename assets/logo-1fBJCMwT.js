16c946f2ee636723fb049da41efe2bddee574e09
omardendani <omardendani@users.noreply.github.com>
2025-08-28 23:27:35 +0000

    deploy: dd2aee594e08d016910d85441c27f1ac345e6289

--- assets/logo-1fBJCMwT.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./index-Ch0d5zCX.js";import n from"./paragraph-DTGlgm-x.js";function i({icon:o,childrenClass:a="btns-st1-ic"}){return s.jsxs("div",{className:"logo-type-1",children:[s.jsx("span",{className:"logo-prefix",children:"Dr."}),s.jsx("div",{className:"logo-name",children:s.jsx(n,{content:`Khawla
-Dendani`})})]})}export{i as Logo};
---
+++ assets/paragraph-BV0Lw3Lo.js
@@ -0,0 +1,3 @@
+import{j as t,R as n}from"./index-CSHwSIrG.js";function x({content:r}){if(r!=null)return(Array.isArray(r)?r.join(`
+`):String(r)).split(`
+`).map((a,s)=>t.jsxs(n.Fragment,{children:[a,t.jsx("br",{})]},s))}export{x as default};
