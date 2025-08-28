98cc7206cf2fafc7527f6177f332abf2d007435a
Omar Dendani <omardendani2003@outlook.com>
2025-08-28 16:47:42 +0100

    Add vite.config.js with base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/olatree-v1/", // <-- nom exact de ton repo GitHub
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: undefined, // Évite les problèmes de chunking
-            }
-        }
-    }
+    base: "/", // <-- nom exact de ton repo GitHub
 });
