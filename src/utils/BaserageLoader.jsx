b7da2d9059d07166944b60551c5e2908149f03f6
Omar Dendani <omardendani2003@outlook.com>
2025-08-28 17:33:54 +0100

    Fix dynamic imports for Baserage routes

--- src/utils/BaserageLoader.jsx
+++ src/utils/BaserageLoader.jsx
@@ -16,7 +16,7 @@ export default function BaserageLoader({ templateName, children }) {
         const link = document.createElement("link");
         link.id = linkId;
         link.rel = "stylesheet";
-        link.href = `/public/view/Baserage/${templateName}/styles/style.css`;
+        link.href = `/view/Baserage/${templateName}/styles/style.css`;
         document.head.appendChild(link);
 
         // Nettoyage lorsque le composant est démonté
